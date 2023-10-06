236f819e7dd91ec1a2a541172a462f1b23a6e884
Roberto David Morales Ramos <rdmr.117@outlook.com>
2023-10-06 16:56:26 -0600

    added anime list app in projects page

--- routers/projects.py
+++ routers/projects.py
@@ -9,3 +9,10 @@ async def projects():
     template_path = "templates/projects.html"
     media_type = "text/html"
     return FileResponse(template_path, media_type=media_type)
+
+
+@router.get("/anime-list-icon.png", response_class=FileResponse, status_code=status.HTTP_200_OK)
+async def anime_list_icon():
+    template_path = "static/images/anime-list-icon.png"
+    media_type = "image/png"
+    return FileResponse(template_path, media_type=media_type)
