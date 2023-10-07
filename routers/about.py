8cb917d1fae19e94c78a1ea042366d42d2822cea
Roberto David Morales Ramos <rdmr.117@outlook.com>
2023-10-06 20:43:29 -0600

    added profile image in about page

--- routers/about.py
+++ routers/about.py
@@ -9,3 +9,10 @@ async def about():
     template_path = "templates/about.html"
     media_type = "text/html"
     return FileResponse(template_path, media_type=media_type)
+
+
+@router.get("/me.png", response_class=FileResponse, status_code=status.HTTP_200_OK)
+async def me():
+    template_path = "static/images/me.png"
+    media_type = "image/png"
+    return FileResponse(template_path, media_type=media_type)
